0d3a0c92b615e878aa06ff83eb0f63fda90c7056
Johnathan <john@knowledgecatalyst.io>
2024-10-14 14:54:42 +0800

    update branding

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 export enum URLS {
   INFO = "https://knowledgecatalyst.io",
   REF = "https://tradetrust-verify.tradetrust.io",
-  CREATOR = "https://tradetrust-creator.knowledgecatalyst.io/",
+  CREATOR = "https://creator.tradestreamx.io/",
   GITHUB = "https://github.com/TradeTrust/tradetrust-website",
   DOCS = "https://docs.tradetrust.io/",
 }
