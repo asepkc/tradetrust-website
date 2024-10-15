f6a1107c312767139e94fe7fccbf40385f21375b
Johnathan <john@knowledgecatalyst.io>
2024-10-15 09:29:53 +0800

    update creator url

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 export enum URLS {
   INFO = "https://knowledgecatalyst.io",
   REF = "https://tradetrust-verify.tradetrust.io",
-  CREATOR = "https://creator.tradestreamx.io/",
+  CREATOR = "https://document-creator.tradestreamx.io/",
   GITHUB = "https://github.com/TradeTrust/tradetrust-website",
   DOCS = "https://docs.tradetrust.io/",
 }
